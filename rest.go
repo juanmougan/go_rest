e79e7ebb1bc75822f232fbcb75f62498f34d5cbd
Juan Manuel Mougan <juan.mougan@mercadolibre.com>
2016-11-09 15:28:34 -0300

    Endpoints for sanity check and JSON marshalling

---
+++ rest.go
@@ -0,0 +1,33 @@
+package main
+ 
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+)
+
+type Message struct {
+    Text string
+}
+ 
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
+}
+
+func about (w http.ResponseWriter, r *http.Request) {
+ 
+    m := Message{"Sample API written by Juan Manuel Mougan"}
+    b, err := json.Marshal(m)
+ 
+    if err != nil {
+        panic(err)
+    }
+ 
+     w.Write(b)
+}
+ 
+func main() {
+    http.HandleFunc("/", index)
+    http.HandleFunc("/about/", about)
+    http.ListenAndServe(":8080", nil)
+}
